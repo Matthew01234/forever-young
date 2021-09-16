e5ea15713e08e046b8ba2ec24fa2b9f0bb9f53d9
Matthew <99067338@mydavinci.nl>
2021-09-16 11:03:50 +0200

    exercise2 klaar

--- robotarm-python-2021-main/robotarm-python-2021-main/exercise2.py
+++ robotarm-python-2021-main/robotarm-python-2021-main/exercise2.py
@@ -3,8 +3,28 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 2')
 
 # Jouw python instructies zet je vanaf hier:
+robotArm.grab()
 
+for i in range(9,0,-1):
+ i * robotArm.moveRight()
 
+robotArm.drop()
+
+for i in range(0,5,+1):
+ i * robotArm.moveLeft()
+robotArm.grab()
+
+for i in range(5,0,-1):
+ i * robotArm.moveRight()
+robotArm.drop()
+
+for i in range(7,9,+1):
+ i * robotArm.moveLeft()
+robotArm.grab()
+
+for i in range(7,0,-1):
+ i * robotArm.moveRight()
+robotArm.drop()
 
 
 
