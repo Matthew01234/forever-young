bafdd34fe49fed1ae5f51c85edc9e2fabab849ac
Matthew <99067338@mydavinci.nl>
2021-09-16 13:38:33 +0200

    level 5

--- robotarm-python-2021-main/robotarm-python-2021-main/exercise4.py
+++ robotarm-python-2021-main/robotarm-python-2021-main/exercise4.py
@@ -3,7 +3,7 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 4')
 
 # Jouw python instructies zet je vanaf hier:
-robotArm.speed(100)
+
 robotArm.grab()
 robotArm.moveRight()
 robotArm.moveRight()
--- robotarm-python-2021-main/robotarm-python-2021-main/exercise5.py
+++ robotarm-python-2021-main/robotarm-python-2021-main/exercise5.py
@@ -5,7 +5,7 @@ robotArm = RobotArm('exercise 5')
 # Jouw python instructies zet je vanaf hier:
 
 
-
+print ('wat moet ik hier doen der staan geen instructies in de online file met uitleg')
 
 
 # Na jouw code wachten tot het sluiten van de window:
