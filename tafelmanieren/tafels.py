112aa4c121c0a816062e831675e95db2bf76e995
Matthew <99067338@mydavinci.nl>
2021-09-16 09:56:06 +0200

    added for loop files

---
+++ tafelmanieren/tafels.py
@@ -0,0 +1,4 @@
+tafel= input ('voer hier de tafel in die je wilt uitrekenen: ')
+
+for i in range(1, 11, 1):
+    print (i * int(tafel))
---
+++ tafelmanieren/urenvandag.py
@@ -0,0 +1,4 @@
+for i in range(1, 13, 1):
+    print('AM ' + str(i))
+for i in range(1, 13, 1):
+       print('PM ' + str(i))
